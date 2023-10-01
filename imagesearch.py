8c7c6de22acbed02c6439110bb5289d93bf261cf
김종인 <enmsoftware@gmail.com>
2023-10-01 10:25:14 +0900

    feat: Unify OS-specific functions

--- imagesearch.py
+++ imagesearch.py
@@ -1,83 +1,54 @@
 import pyautogui
+import platform
+import os
 
 
 class ImageSearch:
-    def __init__(self):
-        self.pos_center = None
+    def __init__(self, default_path: str = None):
+        self.default_path = default_path
+        self.pos = None
         self.detection_status = False
+        self.os_type = None, self.determine_os()
 
-    def get_pos_center(self):
-        return self.pos_center
+    def get_pos(self):
+        return self.pos
 
     def get_detection_status(self):
         return self.detection_status
 
-    def image_search_mac(self, image_name, loop_status=False, click_status=True, click_button='left', confidence=0.85):
-        route = 'images/' + image_name + '.png'
-        print(f'{image_name} detecting...', end='')
-        while True:
-            pos = pyautogui.locateOnScreen(route, confidence=confidence, grayscale=True)
-            if pos is not None:
-                print('successful')
-                self.detection_status = True
-                pos_center = pyautogui.center(pos)
-                self.pos_center = [pos_center[0] / 2, pos_center[1] / 2]
-                if click_status:
-                    if click_button == 'left':
-                        pyautogui.leftClick(self.pos_center)
-                    else:
-                        pyautogui.rightClick(self.pos_center)
-                break
-            else:
-                print('failure')
-            if not loop_status:
-                break
+    def determine_os(self) -> None:
+        self.os_type = platform.system()
+        return
 
-    def image_search_windows(self, image_name, loop_status=False, click_status=True, click_button='left',
-                             confidence=0.95):
-        self.__init__()
-        route = 'images/' + image_name + '.png'
-        failure_count = 0
+    def recombination_path(self, image_name: str) -> str:
+        if self.default_path is None:
+            image_path = image_name
+        else:
+            image_path = os.path.join(self.default_path, image_name)
+        return image_path
+
+    def image_search(self, image_name: str, loop_status: bool = False, click_status: bool = True,
+                     click_button: str = 'left', confidence: float = 0.95) -> bool:
+        self.detection_status = False
+        image_path = self.recombination_path(image_name)
+        count = 0
         while True:
             print(f'{image_name} detecting...', end='')
-            pos = pyautogui.locateOnScreen(route, confidence=confidence, grayscale=True)
-            if pos is not None:
-                print(f'successful [{failure_count}]')
+            self.pos = pyautogui.locateCenterOnScreen(image_path, 0, confidence=confidence, grayscale=True)
+            if self.pos is not None:
+                print(f'successful [{count}]')
                 self.detection_status = True
-                self.pos_center = pyautogui.center(pos)
                 if click_status:
                     if click_button == 'left':
-                        pyautogui.leftClick(self.pos_center)
+                        pyautogui.leftClick(self.pos)
                     else:
-                        pyautogui.rightClick(self.pos_center)
-                break
+                        pyautogui.rightClick(self.pos)
+                return True
             else:
-                failure_count += 1
-                print(f'failure [{failure_count}]', end='')
-                if loop_status:
-                    print('\r', end='')
-                else:
+                count += 1
+                print(f'failure [{count}]', end='')
+                if not loop_status:
                     print()
                     break
-
-    def image_search_twice(self, os_type, image_name01, image_name02, loop_status=False, click_status=True,
-                           click_button='left', confidence=0.85):
-        while True:
-            if os_type == 'mac':
-                self.image_search_mac(image_name01, False, click_status, click_button, confidence)
-                if self.get_pos_center():
-                    break
-                else:
-                    self.image_search_mac(image_name02, False, click_status, click_button, confidence)
-                    if self.get_pos_center():
-                        break
-            elif os_type == 'windows':
-                self.image_search_windows(image_name01, False, click_status, click_button, confidence)
-                if self.get_pos_center():
-                    break
-                else:
-                    self.image_search_windows(image_name02, False, click_status, click_button, confidence)
-                    if self.get_pos_center():
-                        break
-            if not loop_status:
-                break
+                print('\r', end='')
+        return False
